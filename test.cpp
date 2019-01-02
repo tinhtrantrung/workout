d8e020986f82f6d9a76f02786ed79d256e5def89
Tỉnh <tinhtrantrung.vn@gmail.com>
2019-01-02 09:47:59 +0700

    Add comb sort and shell sort. Compile script.

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    cout << "int(7.4999999) = " << int(7.4999999) << endl;
+    cout << "int(7.5) = " << int(7.5) << endl;
+    cout << "int(7.999999)" << int(7.999999) << endl;
+    
+}
