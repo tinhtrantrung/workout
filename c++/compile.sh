283e2a16ad7bdae3a4db0d5d2aeab8ede63c4a9c
Tỉnh <tinhtrantrung.vn@gmail.com>
2019-10-14 11:42:13 +0700

    update build script.

--- c++/compile.sh
+++ c++/compile.sh
@@ -1,1 +1,11 @@
-clang++ -std=c++17 -Wall $1 -o $2
+if [ -z $1 ]
+then
+    echo
+    echo "Usage: $0 <source file>"
+    echo
+    exit 1
+fi
+
+SRC_FILE=$1
+BIN_NAME=$(echo $SRC_FILE | cut -d'.' -f 1)
+clang++ -std=c++17 -Wall $SRC_FILE -o $BIN_NAME
